e74d2d8d768c3aba47a8404d22fd076250e3bf84
rx460 <rx460@naver.com>
2020-09-26 20:20:51 +0900

    9/25일 공부

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import React from 'react';
-
-function Food({fav}) {
+import imgA from './images/1.jpg';
+import imgB from './images/2.jpg';
+import imgC from './images/3.jpg';
+import imgD from './images/4.jpg';
+import imgE from './images/5.jpg';
+function Food({name, images, altText}) {
 
   return (
-    <h3> I love Poatato {fav}</h3>
+      <div>
+        <h3> I love{name}</h3>
+        <img src = {images} alt ={altText} />
+      </div>
   );
 }
 
-function App() {
+const foodLink = [
+{
+  id: 1,
+  name: '김치', altText: '피자를 먹는 모습',
+  
+  image: imgA
+},
+{
+  id: 2,
+  name: '피자', 
+  
+  image: imgB, altText: '피자를 먹는 모습'
+  
+},
+{
+  id: 3, altText: '피자를 먹는 모습',
+  
+  name: '햄버거',
+  image: imgC
 
+},{
+  id: 5, altText: '피자를 먹는 모습',
+  
+  name: '햄버거',
+  image: imgD
+
+},{
+  id: 4,
+  name: '햄버거', altText: '피자를 먹는 모습',
+  
+  image: imgE
+
+}
+
+];
+
+function App() {
     return (
     <div> 
-     <h1> hello!
-        <Food fav="김치" /> 
-        <Food fav="김치" />
-        </h1>
+
+    {foodLink.map(dish => (<Food name = {dish.name}  images = {dish.image} altText= {dish.altText}/>))}
+
     </div>
     );
   }
