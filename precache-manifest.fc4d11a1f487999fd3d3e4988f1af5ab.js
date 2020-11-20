0dabfbf44b7d67185586f800e7c901410cfbb073
rx5460 <rx460@naver.com>
2020-11-20 16:28:01 +0900

    Updates

---
+++ precache-manifest.fc4d11a1f487999fd3d3e4988f1af5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5283812688effc4777d6850a1fb99f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a589d6f2933278a911a4",
+    "url": "/movie_app_2020/static/css/main.91302847.chunk.css"
+  },
+  {
+    "revision": "448a691d6edbd574d672",
+    "url": "/movie_app_2020/static/js/2.5e1e8ba9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.5e1e8ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a589d6f2933278a911a4",
+    "url": "/movie_app_2020/static/js/main.3755fe8b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
